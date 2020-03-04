d292d181e5cc410647994a523147cfef0ea222e7
sergeytata <sergeychirkunov1@gmail.com>
2020-03-04 21:27:22 +0000

    implemeted lambda

--- sc7617/TestFsharp.fsx
+++ sc7617/TestFsharp.fsx
@@ -6,6 +6,7 @@ let map = Map.empty
 
 let mapAdd1 = map.Add("1", "one")
 let mapAdd2 = mapAdd1.Add("2", "two")
+let mapAdd3 = mapAdd2.Add("1", "ONE")
 
 
 let str1 = "Hello"
@@ -13,7 +14,19 @@ let str2 = "World"
 let int1 = 1
 let int2 = 2
 
-print <| not (str1 = str1)
-print <| mapAdd2.["1"]
-print <| mapAdd2.["2"]
-print <| Map.tryFind ("3") mapAdd2
+// print <| not (str1 = str1)
+// print <| mapAdd2.["1"]
+// print <| mapAdd2.["2"]
+// print <| mapAdd3.["1"]
+// print <| Map.tryFind ("3") mapAdd2
+
+
+
+let matchStr (inp: string) = 
+    let strLst = (inp.Split ':') |> Array.toList
+    let expected = "No valid expression supplied for lambda calculation. Evaluated pure lambda is"
+    match strLst with
+    | h :: rest when h = expected -> "Yes"
+    | _ -> "No"
+
+print <| matchStr "No valid expression supplied for lambda calculation. Evaluated pure lambda"
