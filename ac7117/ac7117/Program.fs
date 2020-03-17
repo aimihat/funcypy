4ee916f2eff3a8b38f7260574038feca90303c28
Aimilios Hatzistamou <aimilios@wisepricing.com>
2020-03-17 23:06:00 +0200

    deleting individual folders

--- ac7117/ac7117/Program.fs
+++
@@ -1,8 +0,0 @@
-﻿open Expecto
-
-let expectoConfig = { defaultConfig with verbosity = Logging.LogLevel.Debug }
-
-[<EntryPoint>]
-let main argv =
-    runTestsInAssemblyWithCLIArgs [] [||] |> ignore
-    0
--- jhk2217/Lexer.fs
+++
@@ -1,3 +0,0 @@
-module Combinator_runtime
-
-let deleteThisSample() = printfn "sample top level function in Module1"
