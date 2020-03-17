234b465ff3dd86660ce176ac12efd3a675fe558b
Aimilios Hatzistamou <aimilios@wisepricing.com>
2020-03-17 22:59:27 +0200

    added command line integration and file reading

---
+++ src/Lexer.fs
@@ -0,0 +1,131 @@
+module Lexer
+open Helpers
+
+let intChars = ['0';'1';'2';'3';'4';'5';'6';'7';'8';'9']
+let boolMap = Map ["true",true;"false",false]
+let mathMap = Map ["+", Arithm Add; "-", Arithm Subtract;"*", Arithm Multiply;"/", Arithm Divide;"==",Comp Eq;"!=",Comp Ne;"<",Comp Lt;">",Comp Gt;"<=",Comp Le;">=",Comp Ge]
+let spaceMap = Map [" ",Space;"\f",FormFeed;"\n",LineFeed;"\r",CarriageReturn;"\t",HorizontalTab;"\v",VerticalTab]
+let opMap = Map ["[",LSB;"]",RSB;"(",LRB;",",COMMA;"if",IF;"=",EQUALS;"def",DEF;"then",THEN;"else",ELSE;")",RRB;"lambda",LAMBDA;"->",ARROWFUNC]
+let unaryMap = Map ["not",NOT;"-",NEGATE]
+
+let keys map =
+    map
+    |> Map.toList
+    |> List.map fst
+    
+let rec charstring (clst: char list) =
+    match clst with
+    | hd::tl -> (string hd)+(charstring tl)
+    | _ -> ""
+
+let inMap map el =
+    keys map |> List.exists (fun x -> x = el)
+
+let rec skipSpaces lst =
+    match lst with
+    | hd::tl when hd = ' ' -> skipSpaces tl
+    | newlst -> newlst
+
+let rec skipSpaceToks lst =
+    match lst with
+    | hd::tl when hd = TokWhitespace Space -> skipSpaceToks tl
+    | newlst -> newlst
+
+let printPipe x = printfn "%A" x; x
+
+let tokeniser (str: string) =
+    //buildNum: given that the first char is a number, build either int or float
+    let buildNum (tokLst,otherLst,positive) =
+        let rec buildInts (acc,lst) =
+            match lst with
+            | hd::tl when List.exists (fun el -> el = hd) intChars -> buildInts (acc @ [hd],tl)
+            | _ -> (acc,lst)
+        match buildInts ([],otherLst) with
+        | (intTok,hd::tl) when hd = '.' && positive -> 
+            match buildInts ([],tl) with
+            | ([],tl) -> (tokLst @ [intTok |> charstring |> float |> Double |> TokLit],tl)
+            | (decLst,newOtherLst) -> (tokLst @ [intTok @ ['.'] @ decLst |> charstring |> float |> Double |> TokLit],newOtherLst)
+        | (intTok,newOtherLst) when positive -> (tokLst @ [intTok |> charstring |> int |> Int |> TokLit],newOtherLst)
+        | (intTok,hd::tl) when hd = '.' -> 
+            match buildInts ([],tl) with
+            | ([],tl) -> (tokLst @ [['-'] @ intTok |> charstring |> float |> Double |> TokLit],tl)
+            | (decLst,newOtherLst) -> (tokLst @ [['-'] @ intTok @ ['.'] @ decLst |> charstring |> float |> Double |> TokLit],newOtherLst)
+        | (intTok,newOtherLst) -> (tokLst @ [['-'] @ intTok |> charstring |> int |> Int |> TokLit],newOtherLst)
+    //extractWord: get the next "word", defined by all the characters between two spaces/current space until the end of string
+    let rec extractWord (acc,lst) =
+        match lst with
+        | hd::tl when not (hd = ' ') -> extractWord (acc @ [hd],tl)
+        | hd::tl -> (charstring acc,tl)
+        | [] -> (charstring acc,[])
+    //buildString: a " has been found, build string until another " is found
+    let buildString (tokLst,otherLst) =
+        let rec stringChr (acc,lst) =
+            match lst with
+            | hd::tl when not (hd = '\"') -> stringChr (acc @ [hd],tl)
+            | hd::tl -> (charstring acc |> String |> TokLit,tl)
+            | [] -> (charstring acc |> String |> TokLit,lst) //This should never occur since there is guaranteed to be a quotation mark in lst due to the if-then-else statement
+        let stringOpt (toks,others) =
+            if (List.exists(fun el -> el = '\"') others)
+            then match stringChr ([],others) with (strTok,newOthers) -> Some (toks @ [strTok]), Ok newOthers
+            else None, Error <| sprintf "There is a string that has no ending quotation mark!"
+        match stringOpt (tokLst,otherLst) with
+        | Some newTokLst, Ok newOtherLst -> newTokLst,newOtherLst
+        | None, Ok newOtherLst -> (tokLst @ [TokLit (String "")],newOtherLst) //This should never occur since newTokLst is at the very least the existing list tokLst
+        | _, Error msg -> failwithf "%A" msg
+    let rec tokenise (tokLst,otherLst) =
+        let rec dashID (toks,others) = //must figure out if '-' is negative sign, NEGATE or SUBTRACT
+            match others with
+            | hd::tl when hd = '>' -> (toks @ [TokSpecOp (ARROWFUNC)],tl) // ->
+            | hd::tl when List.exists (fun el -> el = hd) intChars ->
+                match tokLst |> List.rev |> List.head with // #-#
+                | TokLit (Int _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | TokLit (Double _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | _ -> buildNum (toks,others,false)
+            | hd::tl when hd = ' ' ->
+                match skipSpaces tl  with
+                | hd::tl when List.exists (fun el -> el = hd) intChars -> (toks @ [TokBuiltInOp (Arithm Subtract)],others) // - #
+                | '-'::hd::tl when List.exists (fun el -> el = hd) intChars -> (toks @ [TokBuiltInOp (Arithm Subtract)],others) // - -#
+                | hd::tl -> (toks @ [TokUnaryOp (NEGATE)],others)
+                | [] -> //reached end of string
+                    match tokLst |> List.rev |> skipSpaceToks |> List.head with // # -
+                    | TokLit (Int _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                    | TokLit (Double _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                    | TokBuiltInOp _ -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                    | _ -> (toks @ [TokUnaryOp (NEGATE)],others)
+            | hd::tl when inMap mathMap (string hd) -> // - +
+                match tokLst |> List.rev |> List.head with // #-#
+                | TokLit (Int _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | TokLit (Double _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | _ -> buildNum (toks,others,false)
+            | hd1::hd2::tl when List.exists (fun el -> el = charstring [hd1;hd2]) (keys mathMap) -> // - ==
+                match tokLst |> List.rev |> List.head with // #-#
+                | TokLit (Int _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | TokLit (Double _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | _ -> buildNum (toks,others,false)
+            | [] -> //reached end of string
+                match tokLst |> List.rev |> skipSpaceToks |> List.head with // # -
+                | TokLit (Int _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | TokLit (Double _) -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | TokBuiltInOp _ -> (toks @ [TokBuiltInOp (Arithm Subtract)],others)
+                | _ -> (toks @ [TokUnaryOp (NEGATE)],others)
+            | _ -> (toks @ [TokUnaryOp (NEGATE)],others)
+        match otherLst with
+        | hd::tl when hd = ' ' -> tokenise (tokLst @ [TokWhitespace Space],tl)
+        | hd::tl when hd = '-' -> tokenise (dashID (tokLst,tl))
+        | hd::tl when hd = '\"' -> tokenise (buildString (tokLst,tl))
+        | hd::tl when List.exists (fun el -> el = hd) intChars -> tokenise (buildNum (tokLst,otherLst,true))
+        | hd::tl when List.exists (fun el -> el = string hd) (keys mathMap) -> tokenise (tokLst @ [TokBuiltInOp mathMap.[string hd]],tl)
+        | hd1::hd2::tl when List.exists (fun el -> el = charstring [hd1;hd2]) (keys mathMap) -> tokenise (tokLst @ [TokBuiltInOp mathMap.[charstring [hd1;hd2]]],tl)
+        | hd::tl when List.exists (fun el -> el = string hd) (keys opMap) -> tokenise (tokLst @ [TokSpecOp opMap.[string hd]],tl)
+        //hd1::hd2 isn't needed for opMap since ARROWFUNC is taken care of by dashID, also hd1::hd2 might processes IF and IN, which we don't want
+        | hd::tl when List.exists (fun el -> el = string hd) (keys spaceMap) -> tokenise (tokLst @ [TokWhitespace spaceMap.[string hd]],tl)
+        | hd::tl ->
+            match extractWord ([],otherLst) with //true, false, IF, LET, THEN, ELSE, IN, and LAMBDA only works when there is a space before and after
+            | (boolTok,newOtherLst) when inMap boolMap boolTok -> tokenise (tokLst @ [boolMap.[boolTok] |> Bool |> TokLit],newOtherLst)
+            | (mathTok,newOtherLst) when inMap mathMap mathTok -> tokenise (tokLst @ [mathMap.[mathTok] |> TokBuiltInOp],newOtherLst)
+            | (spaceTok,newOtherLst) when inMap spaceMap spaceTok -> tokenise (tokLst @ [spaceMap.[spaceTok] |> TokWhitespace],newOtherLst)
+            | (opTok,newOtherLst) when inMap opMap opTok -> tokenise  (tokLst @ [opMap.[opTok] |> TokSpecOp],newOtherLst)
+            | ("not",newOtherLst) -> tokenise (tokLst @ [NOT |> TokUnaryOp],newOtherLst) // NEGATE is already processed via dashID function
+            | (str,newOtherLst) -> tokenise (tokLst @ [str |> string |> TokIdentifier],newOtherLst)
+        | [] -> tokLst
+    tokenise ([],Seq.toList str)
---
+++ src/Parser.fs
@@ -0,0 +1,353 @@
+module Parser
+
+// open Lexer James module
+
+/// Things that may be used by other modules such as Tokeniser and Runtimes
+/// included in Common Module (e.g. AST, Token definitions)
+open Helpers
+
+/////////////////////////////////////////// PARSER ////////////////////////////////////////////
+
+/// Single Case D.U. used as a wrapper to create a type
+type Parser<'T> = P of (list<Token> -> int -> Option<'T * int>)
+
+/// Basic building block *pToken*: Takes in token list and index *i*; Returns either Some 
+/// tuple of the token at *i* and the incremented *i* or None
+let pToken: Parser<Token> =
+    P <| fun tokenList i ->
+        if i < tokenList.Length then Some(tokenList.[i], i + 1) else None
+
+/// Helper function: Helps to run *aParser* easily
+let pRun (P aParser) tokL = aParser tokL 0
+
+/// Takes a *Token* and always returns Some tuple of *Token* and an unaltered index *i*
+let pReturn tok: Parser<'T> = P <| fun t i -> Some(tok, i)
+
+/// Takes unit and always returns None
+let pFail(): Parser<'T> = P <| fun t i -> None
+
+/// Following standard functional pattern, takes output of one parser and feeds it
+/// as input to another parser. This allows the chaining of parsers together.
+/// *ufunc* is a function that takes a type T and returns a parser of some type U
+/// *tparser* is a parser of the same type U
+let pBind (ufunc: 'T -> Parser<'U>) (P tparser): Parser<'U> =
+    P <| fun tokenList i ->
+        match tparser tokenList i with
+        | None -> None
+        | Some(tvalue, newI) ->
+            let (P uparser) = ufunc tvalue
+            uparser tokenList newI
+
+/// Combines two parsers together
+let pCombine (uParser: Parser<'U>) (tParser: Parser<'T>): Parser<'U> = tParser |> pBind (fun _ -> uParser)
+
+/// Applies two parsers and only keeps result of right parser
+let pKeepRight uParser tParser = pCombine uParser tParser
+
+/// Applies two parsers and only keeps result of left parser
+let pKeepLeft (uParser: Parser<'U>) (tParser: Parser<'T>): Parser<'T> =
+    tParser |> pBind (fun tokenValue -> uParser |> pBind (fun _ -> pReturn tokenValue))
+
+/// Takes parser of a list of *tparser* and returns Some tuple of values
+/// and the index *i* at which the parser fails (if it does!)
+/// Can be used to parse sequences of tokens
+let pMany (P t): Parser<'T list> =
+    P <| fun tokL index ->
+        // define tail recursive "loop"
+        let rec loop lst i =
+            match t tokL i with
+            | None -> Some(List.rev lst, i)
+            | Some(tokValue, tpos) -> loop (tokValue :: lst) tpos
+        loop [] index // call the loop
+
+/// Similar to pMany but requires parsing success at least once
+/// Note: is left associative and loop is tail recursive so optimised for F#
+let pChainlMin1 (term: Parser<'T>) (sep: Parser<'T -> 'T -> 'T>): Parser<'T> =
+    let (P termfun) = term
+    let (P sepfun) = sep
+    P <| fun tok i ->
+        let rec loop aggr currentI =
+            match sepfun tok currentI with
+            | None -> Some(aggr, currentI)
+            | Some(sepCombiner, sepI) ->
+                match termfun tok sepI with
+                | None -> None
+                | Some(termValue, termI) -> loop (sepCombiner aggr termValue) termI
+        match termfun tok i with
+        | None -> None
+        | Some(termValue, termI) -> loop termValue termI
+
+/// F# Computation expression: makes it easier to build more complex parsers
+/// Standard FP pattern using earlier defined building block functions
+type ParserBuilder() =
+    class
+        member x.Bind(t, uf) = pBind uf t // Enables let!
+        member x.Combine(t, u) = pCombine u t // Enables do!
+        member x.Return v = pReturn v // Enables return
+        member x.ReturnFrom p: Parser<'T> = p // Enables return!
+        member x.Zero() = pReturn() // allows if x then expr with no else
+    end
+
+let parser = ParserBuilder()
+
+/// Token -> bool; token type checking functions used for unpacking annotation noise
+let isLiteral (tok: Token) =
+    match tok with
+    | TokLit _ -> true
+    | _ -> false
+
+let isUnaryOp (tok: Token) =
+    match tok with
+    | TokUnaryOp _ -> true
+    | _ -> false
+
+let isOperator (tok: Token) =
+    match tok with
+    | TokSpecOp _ -> true
+    | _ -> false
+
+let isIdentifier (tok: Token) =
+    match tok with
+    | TokIdentifier _ -> true
+    | _ -> false
+
+let isBuiltInOp (tok: Token) =
+    match tok with
+    | TokBuiltInOp _ -> true
+    | _ -> false
+
+/// Parses token and if satisfy evaluates to true then returns token, else returns fail
+let pSatisfy (satisfy: Token -> bool): Parser<Token> =
+    parser {
+        let! tok = pToken
+        if satisfy tok then return tok else return! pFail()
+    }
+
+/// Takes a mapping function that maps a type T to type U and a parser of T
+let pMap mappingFunc tParser =
+    parser {
+        let! tokenType = tParser
+        return mappingFunc tokenType     
+    }
+
+/// Combines two parsers into a Parser of a Pair
+let pPair uParser tParser =
+    parser {
+        let! first = tParser
+        let! second = uParser
+        return first, second 
+    }
+
+/// Combines two parsers such that if uParser fails it tries tParser
+let pOrElse (P uParser) (P tParser) =
+    P <| fun str pos ->
+        match tParser str pos with
+        | None -> uParser str pos
+        | Some(tvalue, tpos) -> Some(tvalue, tpos)
+
+/// (AND combinator) applies first parser to source stream, then applies second to remaining part of stream
+let pAnd (P uParser) (P tParser) =
+    P <| fun str pos ->
+        match tParser str pos with
+        | Some(tvalue, tpos) -> uParser tvalue tpos
+        | _ -> None
+
+/// Define combinators: using static member to attach methods specifically to Parser type
+/// *member* keyword shows that this is a member function (i.e. a method)
+/// After this we can express parsers using combinators to make things even more readable!
+type Parser<'T> with
+    static member (>>=) (t, uf) = pBind uf t
+    static member (>>.) (t, u) = pKeepRight u t
+    static member (.>>) (t, u) = pKeepLeft u t
+    static member (.>>.) (t, u) = pPair u t
+    static member (|>>) (t, m) = pMap m t
+    static member (<|>) (t, u) = pOrElse u t
+    static member (<&>) (t, u) = pAnd u t
+
+/// Similar to pMany but requires 1 or more 'T instead of 0 or more
+let pManyMin1 tparser =
+    parser {
+        let! head = tparser
+        let! tail = pMany tparser
+        return head :: tail 
+    }
+
+/// Skips a specific token given as input
+let pSkipToken tok =
+    parser {
+        let! token = pToken
+        if tok = token then return () else return! pFail()
+    }
+    
+/// Get AST Type from BuiltIn Operator, Literal (Const), Identifier (Var), Special Operators
+let pBuiltInFunc = pSatisfy isBuiltInOp |>> fun tok ->
+    match tok with
+    | TokBuiltInOp op -> op |> BuiltInFunc
+    | _ -> failwith "Expected BuiltInOp but did not receive BuiltInOp"
+
+let pConst = pSatisfy isLiteral |>> fun tok ->
+    match tok with
+    | TokLit(Bool x) -> (Bool x) |> Literal
+    | TokLit(Int x) -> (Int x) |> Literal
+    | TokLit(Double x) -> (Double x) |> Literal
+    | TokLit (String x) -> (String x) |> Literal
+    | _ -> failwith "Expected Literal but did not receive literal"
+
+let pVariable = pSatisfy isIdentifier |>> fun tok ->
+    match tok with
+    | TokIdentifier str -> str |> Variable
+    | _ -> failwith "Expected Identifier but did not receive Identifier"
+
+let pSpecOp = pSatisfy isOperator |>> fun tok -> 
+        match tok with
+        | TokSpecOp op -> op
+        | _ -> failwith "Expected Operator but did not receive Operator"
+
+// Single term in expression is either constant or variable
+let pTerm = pConst <|> pVariable
+
+// pOp Skips the operator and builds an FuncApp AST
+let pOp opTok operator =
+    pSkipToken opTok |>> fun c leftTree rightTree -> DCall(DCall((BuiltInFunc operator), leftTree), rightTree)
+
+let pAdd = pOp (TokBuiltInOp (Arithm Add)) (Arithm Add)
+let pSubtract = pOp (TokBuiltInOp (Arithm Subtract)) (Arithm Subtract)
+let pMultiply = pOp (TokBuiltInOp (Arithm Multiply)) (Arithm Multiply)
+let pDivide = pOp (TokBuiltInOp (Arithm Divide)) (Arithm Divide)
+
+
+let pLessThan = pOp (TokBuiltInOp (Comp Lt)) (Comp Lt)
+let pLessThanOrEq = pOp (TokBuiltInOp (Comp Le)) (Comp Le)
+let pGreaterThan = pOp (TokBuiltInOp (Comp Gt)) (Comp Gt)
+let pGreaterThanOrEq = pOp (TokBuiltInOp (Comp Ge)) (Comp Ge)
+let pEqualTo = pOp (TokBuiltInOp (Comp Eq)) (Comp Eq)
+let pNotEqualTo = pOp (TokBuiltInOp (Comp Ne)) (Comp Ne)
+
+
+// Define precedence of basic BuiltInType operators
+let pAllOp = pAdd <|> pSubtract <|> pMultiply <|> pDivide // not currently being used
+let pmultiOrDivide = pMultiply <|> pDivide
+let paddOrSubtract = pAdd <|> pSubtract
+let pchainMultiDivide = pChainlMin1 pTerm pmultiOrDivide
+let pchainAddSubtract = pChainlMin1 pchainMultiDivide paddOrSubtract
+
+let pCompOps = pLessThan <|> pLessThanOrEq <|> pGreaterThan <|> pGreaterThanOrEq <|> pEqualTo <|> pNotEqualTo
+
+// Top level chained Function application parser with operator precedence integrated
+let pChainedFuncApps = pChainlMin1 pchainAddSubtract pCompOps
+
+/// Top Level AST expression parser that combines everything we've done so far
+/// Can be called with pRun (helper function)
+
+let ignoreList =
+    let reducer list =
+        match list with
+        | [] -> ()
+        | lst -> lst |> List.reduce (fun a b -> ())
+    pMap reducer
+
+let combineLambdas args body =
+    let rec addLambda lambdas definition =
+        match lambdas with
+        | [] -> definition
+        | (Variable hd)::tl -> addLambda tl (Lambda(hd, definition))
+        | _ -> failwithf "Shouldn't happen"
+    addLambda (List.rev args) body
+
+let combineCalls left right =
+    let rec addArgs args body =
+        match args with
+        | [] -> body
+        | hd::tl -> addArgs tl (DCall(body, hd))
+    addArgs right left
+
+let rec pExpr: Parser<Ast> =
+    let pFuncDefExp =
+        parser {
+            do! pSkipToken (TokSpecOp DEF)
+            let! (Variable name) = pVariable
+            let! arguments = pMany pVariable
+            do! pSkipToken (TokSpecOp EQUALS)
+            do! pMany (pSkipToken (TokWhitespace LineFeed)) |> ignoreList
+            let! body = pExpr
+            do! pManyMin1 (pSkipToken (TokWhitespace LineFeed)) |> ignoreList
+            let! expr = pExpr 
+            let definition = combineLambdas arguments body
+            return FuncDefExp(name, definition, expr)
+        }
+    
+    let pBracketed =
+        parser {
+            do! pSkipToken (TokSpecOp LRB)
+            let! e = pExpr
+            do! pSkipToken (TokSpecOp RRB)
+            return e
+        }
+    
+    let pCall = 
+        parser {
+            let! left = pVariable <|> pBracketed // problem is here because it doesnt get pExpr but not sure why
+            let! right = pManyMin1 pExpr
+            return combineCalls left right
+        }
+
+
+    let pLambda =
+        parser {
+            do! pSkipToken (TokSpecOp LAMBDA)
+            let! (Variable id) = pVariable
+            do! pSkipToken (TokSpecOp ARROWFUNC)
+            let! body = pExpr
+            return Lambda(id, body)
+        }
+        
+    // parse full pair
+    let pFullPair =
+        parser {
+            do! pSkipToken (TokSpecOp LSB)
+            let! leftArg = pExpr
+            do! pSkipToken (TokSpecOp COMMA)
+            let! rightArg = pExpr
+            do! pSkipToken (TokSpecOp RSB)
+            return DPair(leftArg, DPair(rightArg, Null))
+        }
+
+    // parse empty pair
+    let pEmptyPair =
+        parser {
+            do! pSkipToken (TokSpecOp LSB)
+            do! pSkipToken (TokSpecOp RSB)
+            return DPair(Null, Null)
+        }
+
+    // parse empty pair
+    let pHalfPair =
+        parser {
+            do! pSkipToken (TokSpecOp LSB)
+            let! arg = pExpr
+            do! pSkipToken (TokSpecOp RSB)
+            return DPair(arg, Null)
+        }
+
+    let pOperatorApp =
+        parser {
+            let! leftTree = pVariable <|> pBracketed
+            let! operator = pBuiltInFunc
+            let! rightList = pManyMin1 (pVariable <|> pConst <|> pBracketed)
+            let rightTree = rightList |> List.reduce (fun acc e -> DCall(acc, e))
+            return DCall(DCall(operator, leftTree), rightTree)
+        }
+
+    let pIfThenElse =
+        parser {
+            do! pSkipToken (TokSpecOp IF)
+            let! condition = pVariable <|> pConst <|> pBracketed
+            do! pSkipToken (TokSpecOp THEN) 
+            let! ifTrue = pExpr
+            do! pSkipToken (TokSpecOp ELSE)
+            let! ifFalse = pExpr
+            return DCall(DCall(DCall(BuiltInFunc IfThenElse, condition), ifTrue), ifFalse)
+        }
+    
+    // even if you remove call from here it still doesnt work
+    pCall <|> pFuncDefExp <|> pLambda <|> pIfThenElse <|> pOperatorApp <|> pBracketed <|> pChainedFuncApps <|> pVariable <|> pFullPair <|> pEmptyPair <|> pHalfPair <|> pConst
