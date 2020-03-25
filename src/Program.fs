97364a45a0243cc67e8bb96fa298fceffbd39957
Yannis Panagis <yannis.panagis1998@gmail.com>
2020-03-25 09:53:32 +0000

    merging to master

--- src/Program.fs
+++ src/Program.fs
@@ -20,7 +20,7 @@ let testCase =
 let testCase1 =
     "def factorial x =\n if (x==0) then 1 else (x*(factorial (x-1)))\n factorial 4"
     
-let RUN_TESTS = false;
+let RUN_TESTS = true;
 
 [<EntryPoint>]
 let main argv =
@@ -32,29 +32,29 @@ let main argv =
         endToEndTestsWithExpecto() |> ignore
         
     // Running file - release
-    let BuiltInCode = loadCode "src/mainlib/builtin.fpy"
-    match argv with 
-    | [|path|] ->
-        let UserCode = loadCode path
-        let CombinedCode =
-            try
-                Some <| BuiltInCode + "\n" + UserCode
-            with
-            | error -> 
-                printf "File not found\n"
-                None
+    // let BuiltInCode = loadCode "src/mainlib/builtin.fpy"
+    // match argv with 
+    // | [|path|] ->
+    //     let UserCode = loadCode path
+    //     let CombinedCode =
+    //         try
+    //             Some <| BuiltInCode + "\n" + UserCode
+    //         with
+    //         | error -> 
+    //             printf "File not found\n"
+    //             None
         
-        //Append built-in definitions to user code
-        match CombinedCode with
-        | Some code ->
-            let CodeNoComments = code |> removeComments
-            printf "\n\"\"\"\n%s\n\"\"\"\n" UserCode
-            let result = CodeNoComments |> Tokenise |> Parse |> Interpret
-            let prettyOutput = result |> Option.map PrintTree
-            match prettyOutput with
-            | Some out -> printf "-----------------\n%s\n" out
-            | _ -> printf "No output"
-        | _ -> printf "No code"
-    | _ -> printf "Must enter a .fpy file to execute.\n"
+    //     //Append built-in definitions to user code
+    //     match CombinedCode with
+    //     | Some code ->
+    //         let CodeNoComments = code |> removeComments
+    //         printf "\n\"\"\"\n%s\n\"\"\"\n" UserCode
+    //         let result = CodeNoComments |> Tokenise |> Parse |> Interpret
+    //         let prettyOutput = result |> Option.map PrintTree
+    //         match prettyOutput with
+    //         | Some out -> printf "-----------------\n%s\n" out
+    //         | _ -> printf "No output"
+    //     | _ -> printf "No code"
+    // | _ -> printf "Must enter a .fpy file to execute.\n"
 
     0
